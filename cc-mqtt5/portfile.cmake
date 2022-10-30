a37cc1fd869f01b7116833134ce5aed3c70b949a
Alex Robenko <arobenko@gmail.com>
2022-10-31 08:20:28 +1000

    Upgraded cc-mqtt5 to v2.0

--- cc-mqtt5/portfile.cmake
+++ cc-mqtt5/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO commschamp/cc.mqtt5.generated
-    REF v1.1
-    SHA512 706ec019281c84aba6f1d3731917dd4d68b0402e64712306bea93ad8ffbf169503cc05405fbc6965fe7fc0d03c5c844e7b0970b096cb6c8d4727c221fd88d452
+    REF v2.0
+    SHA512 e91a4b1871a5a80f3123c1b41ff905d1dee7bedfcb88a41e7d52816183450e08b7606b7e38badc6d15a928e3a2f91c301e60e6e10af7ed54590d6bfc63629577
     HEAD_REF master
 )
 
@@ -11,7 +11,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME mqtt311 CONFIG_PATH lib/mqtt5/cmake)
+vcpkg_cmake_config_fixup(PACKAGE_NAME cc_mqtt5 CONFIG_PATH lib/cc_mqtt5/cmake)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
