dea17827c612ee9b84df602bceb7718854db1013
Alex Robenko <arobenko@gmail.com>
2023-12-09 10:57:25 +1000

    New port:
    cc-mqtt5-libs - v0.1

---
+++ ports/cc-mqtt5-libs/portfile.cmake
@@ -0,0 +1,27 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO commschamp/cc.mqtt5.libs
+    REF v0.1
+    SHA512 be2b48e596f42754904a70cb9d5ae770113a2f5fc7942f97a1b332cd3580d5e6748d857820c30ca0be5b7435256ecf1f56564b94e60025d0c0a636470f0ea935
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DCC_MQTT5_CLIENT_DEFAULT_LIB=ON
+        -DCC_MQTT5_CLIENT_APPS=OFF
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME cc_mqtt5_client CONFIG_PATH lib/cc_mqtt5_client/cmake)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" @ONLY)
