73a6181c67b9cf80584167cf3c9261d0c33b6c39
Alex Robenko <arobenko@gmail.com>
2024-04-29 08:12:33 +1000

    Updates:
    * cc-mqtt5-libs - v0.5
    * cc-mqtt311-libs - v0.1 (new)

--- ports/cc-mqtt311-libs/portfile.cmake
+++ ports/cc-mqtt311-libs/portfile.cmake
@@ -1,21 +1,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO commschamp/cc.mqtt5.libs
-    REF v0.5
-    SHA512 cc0785547d1c0505770003b12ae22292cd86fd6a96447c324bd311568ed6aa1ac5ca38f8fe58fe0b4aaaeae7e99f54b1b8a756ee6d81c3c1d48c415c118d28f7
+    REPO commschamp/cc.mqtt311.libs
+    REF v0.1
+    SHA512 889da82c414cb2235f4866f20029fecdf306b7a6a01afc92f927a7832dd17d4334815bd00196cef3b6041f1ff16c6e073161c090359ca87e4bac8204fe09081a
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DCC_MQTT5_CLIENT_DEFAULT_LIB=ON
-        -DCC_MQTT5_CLIENT_APPS=OFF
+        -DCC_MQTT311_CLIENT_DEFAULT_LIB=ON
+        -DCC_MQTT311_CLIENT_APPS=OFF
 )
 
 vcpkg_cmake_install()
 
-vcpkg_cmake_config_fixup(PACKAGE_NAME cc_mqtt5_client CONFIG_PATH lib/cc_mqtt5_client/cmake)
+vcpkg_cmake_config_fixup(PACKAGE_NAME cc_mqtt311_client CONFIG_PATH lib/cc_mqtt311_client/cmake)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
