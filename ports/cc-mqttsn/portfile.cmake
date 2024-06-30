38fd45aef492019e96c11fad728c1e3fdcc8afbf
Alex Robenko <arobenko@gmail.com>
2024-06-30 10:50:34 +1000

    Updates: * comms - v5.2.5 * commsdsl - v6.3.3 * cc.mqtt311.generated - v2.8 * cc.mqttsn.generated - v2.8 * cc.mqtt5.generated - v2.8 * cc.ublox.generated - v2.8 * cc.x509.generated - v1.0 * cc.mqtt5.libs - v1.0 * cc.mqtt311.libs - v1.0

--- ports/cc-mqttsn/portfile.cmake
+++ ports/cc-mqttsn/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO commschamp/cc.mqttsn.generated
-    REF v2.7
-    SHA512 909e227c8b727d33b4515f8070d8901787723e907fdba68c6ccdd5bac8b56b10d0cd3a05036aeacb4c5b54b5e5be54c3070c8aee9bb72aeb50d412109d820591
+    REF v2.8
+    SHA512 01edb386546e50648c92a40a63c7926da7a6143c65eaa54f72c8b6ce812c34e120367701c56d622db349f9382717b7723b5f68af3cecb10ae476119d3a4abf8f
     HEAD_REF master
 )
 
