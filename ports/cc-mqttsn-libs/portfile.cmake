783e23271debffa8f0d663a9b893e884212d4eb8
Alex Robenko <arobenko@gmail.com>
2024-09-04 09:04:14 +1000

    Updates:
    * comms - v5.2.6
    * cc.mqttsn.generated - v2.8.1
    * cc.mqttsn.libs - v2.0.2

--- ports/cc-mqttsn-libs/portfile.cmake
+++ ports/cc-mqttsn-libs/portfile.cmake
@@ -1,16 +1,16 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO commschamp/cc.mqttsn.libs
-    REF v1.0.8
-    SHA512 666119eeb2e9a96f16c4d650b08dc1f049aeb20ecfd31f088c7d0dc9dfa48a740c761fca158782af13082aa9ac64c373257cb7bcb77f9798b073c53588f61707
+    REF v2.0.2
+    SHA512 6f0f3d7207a6b92daba10c3f9016eb1647e7f5d803277dfddccc3128a733727ac0ee0497cbb79eba8c1354fe0e09401968d8a0101a4405a88d7cc27acfcadc19
     HEAD_REF master
 )
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-        -DCC_MQTTSN_BUILD_CLIENT_APPS=OFF
-        -DCC_MQTTSN_BUILD_GATEWAY_APPS=OFF
+        -DCC_MQTTSN_CLIENT_APPS=OFF
+        -DCC_MQTTSN_GATEWAY_APPS=OFF
 )
 
 vcpkg_cmake_install()
@@ -18,9 +18,7 @@ vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME cc_mqttsn_client CONFIG_PATH lib/cc_mqttsn_client/cmake)
 vcpkg_cmake_config_fixup(PACKAGE_NAME cc_mqttsn_gateway CONFIG_PATH lib/cc_mqttsn_gateway/cmake)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/share/doc")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
 
